77c55eb13a70ef4f7b8e5726782910d49c7d3fc6
JacobWylie <jacobwylie@gmail.com>
2017-03-16 14:03:38 +0100

    added error even handling

--- app.js
+++ app.js
@@ -11,21 +11,26 @@ const printMessage = (username, badgeCount, points) => {
 
 
 const getProfile = (username) => {
-	//  Connect to the API URL (https://teamtreehouse.com/username.json)
-	const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-		let body = "";
-		// Read the data
-		response.on('data', data => {
-			body += data.toString();
-		});
+	try {
+		//  Connect to the API URL (https://teamtreehouse.com/username.json)
+		const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+			let body = "";
+			// Read the data
+			response.on('data', data => {
+				body += data.toString();
+			});
 
-		response.on('end', () => {
-		// Parse the data	
-			const profile = JSON.parse(body);
-		// Print the data		
-			printMessage(username, profile.badges.length, profile.points.JavaScript);
+			response.on('end', () => {
+			// Parse the data	
+				const profile = JSON.parse(body);
+			// Print the data		
+				printMessage(username, profile.badges.length, profile.points.JavaScript);
+			});
 		});
-	});
+		request.on('error', error => console.error(`Problem with request: ${error.message}`));
+	} catch (error) {
+		console.error(error.message);
+	}
 };
 
 
