eeb5bf8b087f3077daa7285f7fdc5a8b7d56df06
JacobWylie <jacobwylie@gmail.com>
2017-03-16 13:53:20 +0100

    added command line arguments with process.argv

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
 const https = require('https');
-const username = "chalkers";
 
 // Function to print message to console
 const printMessage = (username, badgeCount, points) => {
@@ -10,18 +9,26 @@ const printMessage = (username, badgeCount, points) => {
 	console.log(message);
 }
 
-//  Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-	let body = "";
-	// Read the data
-	response.on('data', data => {
-		body += data.toString();
-	});
 
-	response.on('end', () => {
-	// Parse the data	
-		const profile = JSON.parse(body);
-	// Print the data		
-		printMessage(username, profile.badges.length, profile.points.JavaScript);
+const getProfile = (username) => {
+	//  Connect to the API URL (https://teamtreehouse.com/username.json)
+	const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+		let body = "";
+		// Read the data
+		response.on('data', data => {
+			body += data.toString();
+		});
+
+		response.on('end', () => {
+		// Parse the data	
+			const profile = JSON.parse(body);
+		// Print the data		
+			printMessage(username, profile.badges.length, profile.points.JavaScript);
+		});
 	});
-});
+};
+
+
+const users = process.argv.slice(2);
+
+users.forEach(getProfile);
